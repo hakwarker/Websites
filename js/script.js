e28fc6cbfc2d97571e3fb5b607f5821b1475d17c
hakwarker <hakwarker@gmail.com>
2026-04-26 19:03:44 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ button.addEventListener('click', function() {
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
 });
+window.onload = function() {
+    const button = document.getElementById('colorButton');
+    
+    if(button) {
+        button.addEventListener('click', function() {
+            const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+            document.body.style.background = randomColor; // ប្តូរមកប្រើ .background វិញដើម្បីឱ្យវា override gradient
+        });
+    }
+};
+
